c3a307b50cb38472ed8b3655b359fa490dfddae2
saumyshah08 <saumyshah08@gmail.com>
2026-09-02 09:31:21 +0530

    Update nav.js

--- nav/nav.js
+++ nav/nav.js
@@ -1,26 +1,11 @@
-(function () {
-  'use strict';
 
-  function initNav() {
-    const header = document.getElementById('siteNavHeader');
-    if (!header) return;
 
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 30) {
-        header.classList.add('is-scrolled');
-      } else {
-        header.classList.remove('is-scrolled');
-      }
-    }, { passive: true });
-  }
+let navHeader = document.querySelector("#siteNavHeader");
 
-  window.Nav = {
-    init: initNav
-  };
-
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', initNav);
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 30) {
+    navHeader.classList.add("is-scrolled");
   } else {
-    initNav();
+    navHeader.classList.remove("is-scrolled");
   }
-})();
+});
